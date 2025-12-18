f65a21c4ee340a003bd871f5e886631940b18df9
Kodsetu <147732840+Kodsetu@users.noreply.github.com>
2025-12-18 19:03:30 +0000

    Update index.js

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -4,15 +4,18 @@ import asyncHandler from 'express-async-handler';
 
 const router = express.Router()// eslint-disable-line
 
-// Get all tasks
+// Get a user's tasks
 router.get('/', async (req, res) => {
-    const tasks = await Task.find().populate('userId', 'username');
+    console.log(req.user);
+    const tasks = await Task.find({ userId: `${req.user._id}`});
     res.status(200).json(tasks);
 });
 
 // create a task
 router.post('/', asyncHandler(async (req, res) => {
-    const task = await Task(req.body).save();
+    const newTask = req.body;
+    newTask.userId = req.user._id;
+    const task = await Task(newTask).save();
     res.status(201).json(task);
 }));
 
@@ -20,6 +23,18 @@ router.post('/', asyncHandler(async (req, res) => {
 router.get('/user/:uid', async (req, res) => {
     const tasks = await Task.find({ userId: `${req.params.uid}`});
     res.status(200).json(tasks);
+});
+
+// delete Task
+router.delete('/:id', async (req, res) => {
+    const result = await Task.deleteOne({
+        _id: req.params.id,
+    });
+    if (result.deletedCount) {
+        res.status(204).json();
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find Task' });
+    }
 });
 
 export default router;
